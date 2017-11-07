07da0276e4a2c898e0da91495afeade75dd95a95
jun <15175848230@163.com>
2017-11-07 21:47:51 +0800

    update

--- ng-train/src/components/common/time_choose/time.js
+++ ng-train/src/components/common/time_choose/time.js
@@ -1,16 +1,13 @@
 import angular from "angular";
 import httpService from "src/components/common/http_service/http_service";
-import "angular-datepicker";
-import "angular-datepicker/dist/index.min.css";
-import "angular-moment-picker/dist/angular-moment-picker";
-import "angular-moment-picker/dist/angular-moment-picker.css";
-import "angular-bootstrap-datetimepicker";
-import "angular-bootstrap-datetimepicker/src/css/datetimepicker.css";
-import "angular-bootstrap-datetimepicker/src/js/datetimepicker.templates.js";
+// import "angular-datepicker";
+// import "angular-datepicker/dist/index.min.css";
+import "angularjs-datepicker";
+import "angularjs-datepicker/dist/angular-datepicker.min.css";
 /*
 * 城市选择
 * */
-export default angular.module("timeChoose", [httpService, "moment-picker", "ui.bootstrap.datetimepicker"])
+export default angular.module("timeChoose", [httpService, "720kb.datepicker"])
     .controller("choose.time", ["$scope", ($scope) => {
         $scope.hideCity = () => {  // 事件 点击隐藏城市选择界面
             $scope.$emit("hideChooseCity", false); // 向父controller传递数据 告诉父组件开始收起城市选择框
@@ -18,5 +15,6 @@ export default angular.module("timeChoose", [httpService, "moment-picker", "ui.b
         $scope.minDate = moment().format('YYYY-MM-DD');
         $scope.maxDate = moment().subtract(30, "day").format('YYYY-MM-DD');
         // $scope.startDate = 
+        $scope.da = new Date()
     }])
     .name;
